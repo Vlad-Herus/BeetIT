ce10e0d7c7b3d277bbd7b86eecbea18ac99af152
Vlad-Herus <VladimirGerusUA@gmail.com>
2021-07-01 15:16:29 +1000

    initial commit

---
+++ BeetIT/Player/Class1.cs
@@ -0,0 +1,42 @@
+﻿using NAudio.Wave;
+using System;
+using System.IO;
+using System.Threading;
+
+namespace Player
+{
+    public class AudioPlayer
+    {
+
+        public void PlaySound(string path)
+        {
+            for (int n = -1; n < WaveOut.DeviceCount; n++)
+            {
+                var caps = WaveOut.GetCapabilities(n);
+                Console.WriteLine($"{n}: {caps.ProductName}");
+            }
+            //using (var file = System.IO.File.OpenRead(path))
+            //{
+            //    PlaySound(file);
+            //}
+        }
+
+        public void PlaySound(Stream audioStream)
+        {
+            using (var rdr = new Mp3FileReader(audioStream))
+            using (var wavStream = WaveFormatConversionStream.CreatePcmStream(rdr))
+            using (var baStream = new BlockAlignReductionStream(wavStream))
+            using (var waveOut = new WaveOut(WaveCallbackInfo.FunctionCallback()))
+            {
+                waveOut.Init(baStream);
+                waveOut.Play();
+                while (waveOut.PlaybackState == PlaybackState.Playing)
+                {
+                    Thread.Sleep(100);
+                }
+            }
+        }
+
+
+    }
+}
---
+++ BeetIT/tester/Program.cs
@@ -0,0 +1,14 @@
+﻿using Player;
+using System;
+using System.IO;
+
+namespace tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new AudioPlayer().PlaySound(@"D:\Music\Lustra - Scotty Doesn't Know.mp3");
+        }
+    }
+}
