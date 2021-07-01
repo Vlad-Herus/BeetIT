e121255b462275a29edf78c3ce4404369b6fa66d
Leslie-SettleIT-Infotrack <84691304+Leslie-SettleIT-Infotrack@users.noreply.github.com>
2021-07-01 16:35:22 +1000

    inistial hotkeys

---
+++ HotkeyManager/HotkeyManager/Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using Key;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Key.HotKeyManager.RegisterHotKey(Keys.PageUp, KeyModifiers.None, new Action<Keys, KeyModifiers>((key, mod) =>
+            {
+                Console.WriteLine("aaaa");
+            }));
+        }
+    }
+}
